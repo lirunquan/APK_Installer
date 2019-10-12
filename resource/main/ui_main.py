eace74cf3d1512114193255bed510d9d0865393b
lirunquan <lirunquan@3k.com>
2019-10-12 10:16:39 +0800

    fix uis

--- resource/main/ui_main.py
+++ resource/main/ui_main.py
@@ -13,15 +13,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Main_Form(object):
     def setupUi(self, Main_Form):
         Main_Form.setObjectName("Main_Form")
-        Main_Form.resize(418, 479)
+        Main_Form.resize(440, 500)
         self.widget_devices = QtWidgets.QWidget(Main_Form)
-        self.widget_devices.setGeometry(QtCore.QRect(0, 0, 411, 201))
+        self.widget_devices.setGeometry(QtCore.QRect(10, 0, 420, 221))
         self.widget_devices.setObjectName("widget_devices")
         self.widget_apk = QtWidgets.QWidget(Main_Form)
-        self.widget_apk.setGeometry(QtCore.QRect(0, 200, 411, 231))
+        self.widget_apk.setGeometry(QtCore.QRect(10, 220, 420, 231))
         self.widget_apk.setObjectName("widget_apk")
         self.pushButton_install = QtWidgets.QPushButton(Main_Form)
-        self.pushButton_install.setGeometry(QtCore.QRect(160, 440, 80, 24))
+        self.pushButton_install.setGeometry(QtCore.QRect(170, 460, 80, 24))
         self.pushButton_install.setObjectName("pushButton_install")
 
         self.retranslateUi(Main_Form)
